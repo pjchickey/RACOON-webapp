7226d5b757bba6c61b2885ddf87f2be754ba0df8
Patrick Chickey <pjc8@rice.edu>
2021-03-23 03:04:07 -0500

    changed 'postgres' to 'postgresql' in manual URL :((

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.urandom(24)
     # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_DATABASE_URI = 'postgres://iyrxjafwmybqog:047647631db0ac1b3d727d7edd5b9e4c299586131585e1b6d41b2bc98e412521@ec2-52-7-115-250.compute-1.amazonaws.com:5432/dfmfctp63eg654'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://iyrxjafwmybqog:047647631db0ac1b3d727d7edd5b9e4c299586131585e1b6d41b2bc98e412521@ec2-52-7-115-250.compute-1.amazonaws.com:5432/dfmfctp63eg654'
