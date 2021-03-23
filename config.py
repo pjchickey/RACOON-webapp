52c02629fe212ca489c5aa4a2e2064f08460600f
Patrick Chickey <pjc8@rice.edu>
2021-03-23 02:51:30 -0500

    trying more postgres things

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
